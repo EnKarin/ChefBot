ec43b7a194fc26f227d4c822b6205816298e2c3f
Aleksey <alleksey-Kn@yandex.ru>
2024-08-18 23:51:32 +0700

    Added DishEntityDtoMapperTest

---
+++ src/test/java/io/github/enkarin/chefbot/mappers/DishEntityDtoMapperTest.java
@@ -0,0 +1,20 @@
+package io.github.enkarin.chefbot.mappers;
+
+import io.github.enkarin.chefbot.entity.Product;
+import io.github.enkarin.chefbot.util.TestBase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DishEntityDtoMapperTest extends TestBase {
+    @Autowired
+    private DishEntityDtoMapper mapper;
+
+    @Test
+    void productToString() {
+        assertThat(mapper.productToString(Product.builder().productName("bread").dishes(Set.of()).build())).isEqualTo("bread");
+    }
+}
