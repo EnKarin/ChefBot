d68beb930d1665830647697ca1dbf546b200d549
Aleksey <alleksey-Kn@yandex.ru>
2024-08-18 23:35:20 +0700

    Added tests into TelegramControllerTest and TelegramAdapterTest

--- src/test/java/io/github/enkarin/chefbot/TelegramAdapterTest.java
+++ src/test/java/io/github/enkarin/chefbot/TelegramAdapterTest.java
@@ -2,11 +2,18 @@ package io.github.enkarin.chefbot;
 
 import io.github.enkarin.chefbot.adapters.TelegramAdapter;
 import io.github.enkarin.chefbot.dto.ModerationDishDto;
+import io.github.enkarin.chefbot.entity.Dish;
+import io.github.enkarin.chefbot.entity.ModerationRequest;
+import io.github.enkarin.chefbot.entity.ModerationRequestMessage;
 import io.github.enkarin.chefbot.entity.User;
 import io.github.enkarin.chefbot.enums.ChatStatus;
+import io.github.enkarin.chefbot.repository.ModerationRequestMessageRepository;
+import io.github.enkarin.chefbot.repository.ModerationRequestRepository;
 import io.github.enkarin.chefbot.util.TestBase;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.MessageEntity;
@@ -20,6 +27,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 class TelegramAdapterTest extends TestBase {
     @Autowired
     private TelegramAdapter telegramAdapter;
+
+    @Autowired
+    private ModerationRequestRepository moderationRequestRepository;
+
+    @Autowired
+    private ModerationRequestMessageRepository moderationRequestMessageRepository;
+
+    private final long[] moderationRequestsId = new long[4];
+
+    @AfterEach
+    void clean() {
+        moderationRequestRepository.deleteAll();
+    }
 
     @Test
     void onUpdateReceivedStartCommand() {
@@ -74,5 +94,62 @@ class TelegramAdapterTest extends TestBase {
         assertThat(telegramAdapter.sendModerationRequests(Set.of(10L, 20L), ModerationDishDto.builder().requestId(666).build())).isNotNull();
     }
 
-    // todo: test callback
+    @Test
+    void approveModerationRequest() {
+        moderationInit();
+
+        telegramAdapter.onUpdateReceived(createUpdateWithCallbackQuery("Одобрить запрос №" + moderationRequestsId[0]));
+
+        assertThat(moderationRequestRepository.existsById(moderationRequestsId[0])).isFalse();
+    }
+
+    @Test
+    void declineModerationRequest() {
+        moderationInit();
+
+        telegramAdapter.onUpdateReceived(createUpdateWithCallbackQuery("Отклонить запрос №" + moderationRequestsId[1]));
+
+        assertThat(moderationRequestRepository.existsById(moderationRequestsId[1])).isFalse();
+    }
+
+    private Update createUpdateWithCallbackQuery(final String data) {
+        final Update update = new Update();
+        final CallbackQuery callbackQuery = new CallbackQuery();
+        callbackQuery.setData(data);
+        update.setCallbackQuery(callbackQuery);
+        return update;
+    }
+
+    private void moderationInit() {
+        userService.createOrUpdateUser(USER_ID, CHAT_ID, USERNAME);
+        final User user = userRepository.save(User.builder().moderator(true).id(USER_ID - 1).chatId(CHAT_ID - 1).username(USERNAME + 1).build());
+        final ModerationRequest firstModerationRequest = moderationRequestRepository.save(ModerationRequest.builder()
+                .fresh(false)
+                .moderationDish(dishRepository.save(Dish.builder().owner(user).dishName("firstDish").build()))
+                .build());
+        moderationRequestMessageRepository.saveAll(List.of(ModerationRequestMessage.builder().messageId(1).chatId(10).currentModerationRequest(firstModerationRequest).build(),
+                ModerationRequestMessage.builder().messageId(1).chatId(11).currentModerationRequest(firstModerationRequest).build()));
+        moderationRequestsId[0] = firstModerationRequest.getId();
+        final ModerationRequest secondModerationRequest = moderationRequestRepository.save(ModerationRequest.builder()
+                .fresh(false)
+                .moderationDish(dishRepository.save(Dish.builder().owner(user).dishName("secondDish").build()))
+                .build());
+        moderationRequestMessageRepository.saveAll(List.of(ModerationRequestMessage.builder().messageId(2).chatId(20).currentModerationRequest(secondModerationRequest).build(),
+                ModerationRequestMessage.builder().messageId(2).chatId(22).currentModerationRequest(secondModerationRequest).build()));
+        moderationRequestsId[1] = secondModerationRequest.getId();
+        final ModerationRequest thirdModerationRequest = moderationRequestRepository.save(ModerationRequest.builder()
+                .fresh(true)
+                .moderationDish(dishRepository.save(Dish.builder().owner(user).dishName("thirdDish").build()))
+                .build());
+        moderationRequestMessageRepository.saveAll(List.of(ModerationRequestMessage.builder().messageId(3).chatId(30).currentModerationRequest(thirdModerationRequest).build(),
+                ModerationRequestMessage.builder().messageId(3).chatId(33).currentModerationRequest(thirdModerationRequest).build()));
+        moderationRequestsId[2] = thirdModerationRequest.getId();
+        final ModerationRequest fourthModerationRequest = moderationRequestRepository.save(ModerationRequest.builder()
+                .fresh(true)
+                .moderationDish(dishRepository.save(Dish.builder().owner(user).dishName("fourthDish").build()))
+                .build());
+        moderationRequestMessageRepository.saveAll(List.of(ModerationRequestMessage.builder().messageId(4).chatId(40).currentModerationRequest(fourthModerationRequest).build(),
+                ModerationRequestMessage.builder().messageId(4).chatId(44).currentModerationRequest(fourthModerationRequest).build()));
+        moderationRequestsId[3] = fourthModerationRequest.getId();
+    }
 }
--- src/test/java/io/github/enkarin/chefbot/TelegramControllerTest.java
+++ src/test/java/io/github/enkarin/chefbot/TelegramControllerTest.java
@@ -2,21 +2,41 @@ package io.github.enkarin.chefbot;
 
 import io.github.enkarin.chefbot.controllers.TelegramController;
 import io.github.enkarin.chefbot.dto.BotAnswer;
+import io.github.enkarin.chefbot.dto.ModerationRequestMessageDto;
 import io.github.enkarin.chefbot.entity.Dish;
+import io.github.enkarin.chefbot.entity.ModerationRequest;
+import io.github.enkarin.chefbot.entity.ModerationRequestMessage;
 import io.github.enkarin.chefbot.entity.User;
 import io.github.enkarin.chefbot.enums.ChatStatus;
 import io.github.enkarin.chefbot.enums.UserAnswerOption;
+import io.github.enkarin.chefbot.repository.ModerationRequestMessageRepository;
+import io.github.enkarin.chefbot.repository.ModerationRequestRepository;
 import io.github.enkarin.chefbot.util.TestBase;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TelegramControllerTest extends TestBase {
-
     @Autowired
     private TelegramController telegramController;
+
+    @Autowired
+    private ModerationRequestRepository moderationRequestRepository;
+
+    @Autowired
+    private ModerationRequestMessageRepository moderationRequestMessageRepository;
+
+    private final long[] moderationRequestsId = new long[4];
+
+    @AfterEach
+    void clean() {
+        moderationRequestRepository.deleteAll();
+    }
 
     @Test
     void executeWorkerCommandStart() {
@@ -98,5 +118,60 @@ class TelegramControllerTest extends TestBase {
         assertThat(telegramController.executeWorkerCommand(USER_ID, "/undo").messageText()).isEqualTo("Отменить действие можно лишь один раз подряд");
     }
 
-    //todo: test callback
+    @Test
+    void approveModerationRequest() {
+        moderationInit();
+
+        assertThat(telegramController.approveModerationRequest("Подтвердить запрос №" + moderationRequestsId[0])).satisfies(moderationResultDto -> {
+            assertThat(moderationResultDto.approve()).isTrue();
+            assertThat(moderationResultDto.dishName()).isEqualTo("firstDish");
+            assertThat(moderationResultDto.messageForRemove()).extracting(ModerationRequestMessageDto::chatId).contains(10L, 11L);
+        });
+        assertThat(moderationRequestRepository.existsById(moderationRequestsId[0])).isFalse();
+    }
+
+    @Test
+    void declineModerationRequest() {
+        moderationInit();
+
+        assertThat(telegramController.declineModerationRequest("Отклонить запрос №" + moderationRequestsId[1])).satisfies(moderationResultDto -> {
+            assertThat(moderationResultDto.approve()).isFalse();
+            assertThat(moderationResultDto.dishName()).isEqualTo("secondDish");
+            assertThat(moderationResultDto.messageForRemove()).extracting(ModerationRequestMessageDto::chatId).contains(20L, 22L);
+        });
+        assertThat(moderationRequestRepository.existsById(moderationRequestsId[1])).isFalse();
+    }
+
+    private void moderationInit() {
+        userService.createOrUpdateUser(USER_ID, CHAT_ID, USERNAME);
+        final User user = userRepository.save(User.builder().moderator(true).id(USER_ID - 1).chatId(CHAT_ID - 1).username(USERNAME + 1).build());
+        final ModerationRequest firstModerationRequest = moderationRequestRepository.save(ModerationRequest.builder()
+                .fresh(false)
+                .moderationDish(dishRepository.save(Dish.builder().owner(user).dishName("firstDish").build()))
+                .build());
+        moderationRequestMessageRepository.saveAll(List.of(ModerationRequestMessage.builder().messageId(1).chatId(10).currentModerationRequest(firstModerationRequest).build(),
+                ModerationRequestMessage.builder().messageId(1).chatId(11).currentModerationRequest(firstModerationRequest).build()));
+        moderationRequestsId[0] = firstModerationRequest.getId();
+        final ModerationRequest secondModerationRequest = moderationRequestRepository.save(ModerationRequest.builder()
+                .fresh(false)
+                .moderationDish(dishRepository.save(Dish.builder().owner(user).dishName("secondDish").build()))
+                .build());
+        moderationRequestMessageRepository.saveAll(List.of(ModerationRequestMessage.builder().messageId(2).chatId(20).currentModerationRequest(secondModerationRequest).build(),
+                ModerationRequestMessage.builder().messageId(2).chatId(22).currentModerationRequest(secondModerationRequest).build()));
+        moderationRequestsId[1] = secondModerationRequest.getId();
+        final ModerationRequest thirdModerationRequest = moderationRequestRepository.save(ModerationRequest.builder()
+                .fresh(true)
+                .moderationDish(dishRepository.save(Dish.builder().owner(user).dishName("thirdDish").build()))
+                .build());
+        moderationRequestMessageRepository.saveAll(List.of(ModerationRequestMessage.builder().messageId(3).chatId(30).currentModerationRequest(thirdModerationRequest).build(),
+                ModerationRequestMessage.builder().messageId(3).chatId(33).currentModerationRequest(thirdModerationRequest).build()));
+        moderationRequestsId[2] = thirdModerationRequest.getId();
+        final ModerationRequest fourthModerationRequest = moderationRequestRepository.save(ModerationRequest.builder()
+                .fresh(true)
+                .moderationDish(dishRepository.save(Dish.builder().owner(user).dishName("fourthDish").build()))
+                .build());
+        moderationRequestMessageRepository.saveAll(List.of(ModerationRequestMessage.builder().messageId(4).chatId(40).currentModerationRequest(fourthModerationRequest).build(),
+                ModerationRequestMessage.builder().messageId(4).chatId(44).currentModerationRequest(fourthModerationRequest).build()));
+        moderationRequestsId[3] = fourthModerationRequest.getId();
+    }
 }
