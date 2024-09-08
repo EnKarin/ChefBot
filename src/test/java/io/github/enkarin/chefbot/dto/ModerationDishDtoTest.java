4315f50f30faa9060022a5794fcb787090a6cb41
Aleksey <alleksey-Kn@yandex.ru>
2024-09-08 21:40:10 +0700

    Fix ModerationDishDto.toString

--- src/test/java/io/github/enkarin/chefbot/dto/ModerationDishDtoTest.java
+++ src/test/java/io/github/enkarin/chefbot/dto/ModerationDishDtoTest.java
@@ -9,16 +9,16 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ModerationDishDtoTest {
-
     @Test
-    void testToString() {
+    void toStringWithoutRecipe() {
         final String stringDto = ModerationDishDto.builder()
                 .spicy(false)
                 .worldCuisine(WorldCuisine.INTERNATIONAL)
                 .type(DishType.SOUP)
                 .name("Бульон")
                 .products(Set.of("Курица", "Соль", "Вода"))
-                .build().toString();
+                .build()
+                .toString();
         assertThat(stringDto).startsWith("""
                 *Бульон*
                 Острое: нет
@@ -26,5 +26,26 @@ class ModerationDishDtoTest {
                 Кухня: международная
                 Состав:""");
         assertThat(stringDto).contains("Курица", "Вода", "Соль");
+    }
+
+    @Test
+    void toStringWithRecipe() {
+        final String stringDto = ModerationDishDto.builder()
+                .spicy(false)
+                .worldCuisine(WorldCuisine.INTERNATIONAL)
+                .type(DishType.SOUP)
+                .name("Бульон")
+                .products(Set.of("Курица", "Лапша"))
+                .recipe("Варить полчаса")
+                .build()
+                .toString();
+        assertThat(stringDto).startsWith("""
+                *Бульон*
+                Острое: нет
+                Тип: суп
+                Кухня: международная
+                Состав:""");
+        assertThat(stringDto).contains("Курица", "Лапша");
+        assertThat(stringDto).endsWith("Рецепт: Варить полчаса");
     }
 }
