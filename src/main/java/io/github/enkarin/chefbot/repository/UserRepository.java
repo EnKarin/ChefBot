08774ff6e1513e49aca2e0f0fff3d238dbf6d4ff
Aleksey <alleksey-Kn@yandex.ru>
2024-09-23 00:32:35 +0700

    Create DishService.deleteDish

--- src/main/java/io/github/enkarin/chefbot/repository/UserRepository.java
+++ src/main/java/io/github/enkarin/chefbot/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package io.github.enkarin.chefbot.repository;
 
+import io.github.enkarin.chefbot.entity.Dish;
 import io.github.enkarin.chefbot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Set<User> findAllByModeratorIsTrueAndChatIdIsNot(long id);
+
+    List<User> findAllByModerableDish(Dish dish);
 }
